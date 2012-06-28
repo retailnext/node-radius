8a3f6e5480e3aeef41877627eb109955f22e5df5
Muir Manders <muir@nearbuysystems.com>
2012-06-28 09:58:34 -0700

    Add "raw_attributes" for untranslated attributes.
    Regardless of if you have the dictionary to parse the attributes,
    "raw_attributes" will always have all the attributes from the packet.
    It is an array of [type, Buffer] tuples, where "type" is the attribute
    type, and "Buffer" is the attribute value.

--- test/parse.test.js
+++ test/parse.test.js
@@ -4,13 +4,14 @@ var fs = require('fs');
 
 var secret = 'nearbuy';
 
-radius.load_dictionaries();
 
 module.exports = testCase({
   setUp: function(callback) {
+    radius.load_dictionaries();
     callback();
   },
   tearDown: function(callback) {
+    radius.unload_dictionaries();
     callback();
   },
 
@@ -44,5 +45,41 @@ module.exports = testCase({
     test.deepEqual( expected_attrs, parsed.attributes );
 
     test.done();
+  },
+
+  // make sure everthing is fine with no dictionaries
+  test_parse_no_dicts: function(test) {
+    var raw_packet = fs.readFileSync(__dirname + '/captures/aruba_mac_auth.packet');
+
+    radius.unload_dictionaries();
+
+    var parsed = radius.parse(raw_packet, secret);
+
+    test.equal( 'Access-Request', parsed.code );
+    test.equal( 58, parsed.identifier );
+    test.equal( 208, parsed.length );
+
+    // no pretty attributes
+    test.deepEqual( {}, parsed.attributes );
+
+    var expected_raw_attrs = [
+      [4, new Buffer([10, 0, 0, 90])],
+      [5, new Buffer([0, 0, 0, 0])],
+      [61, new Buffer([0, 0, 0, 19])],
+      [1, new Buffer('7c:c5:37:ff:f8:af')],
+      [2, new Buffer('eb2ef7e83ec1a05e04fb5c6d91e088569a990fa2b1b2dc6a0f048596081164cd', 'hex')],
+      [31, new Buffer('7CC537FFF8AF')],
+      [30, new Buffer('000B86F02068')],
+      [6, new Buffer([0, 0, 0, 1])],
+      [26, new Buffer('000039e705126d7569722d61727562612d6775657374', 'hex')],
+      [26, new Buffer('000039e7061330303a31613a31653a63363a62303a6361', 'hex')],
+      [26, new Buffer('000039e70a0a636c6f75642d6370', 'hex')],
+      [80, new Buffer('f8a12329c7ed5a6e2568515243efb918', 'hex')],
+    ];
+
+    test.deepEqual( expected_raw_attrs, parsed.raw_attributes );
+
+    test.done();
   }
+
 });
