59e787cbb405dda8301f3a6f5904abf5511e8fea
Muir Manders <muir@nearbuysystems.com>
2012-07-01 01:21:02 -0700

    Add support for dictionary $INCLUDEs.

--- test/radius.test.js
+++ test/radius.test.js
@@ -418,5 +418,26 @@ module.exports = testCase({
     test.deepEqual( expected_attrs, decoded.attributes );
 
     test.done();
+  },
+
+  test_dictionary_include: function(test) {
+    radius.load_dictionary('dictionaries/dictionary.test1');
+
+    var decoded = radius.decode({
+      secret: secret,
+      packet: radius.encode({
+        secret: secret,
+        code: 'Access-Request',
+        attributes: [['Attribute-Test1', 'foo'], ['Attribute-Test2', 'bar']]
+      })
+    });
+
+    var expected_attrs = {
+      'Attribute-Test1': 'foo',
+      'Attribute-Test2': 'bar'
+    };
+    test.deepEqual( expected_attrs, decoded.attributes );
+
+    test.done();
   }
 });
