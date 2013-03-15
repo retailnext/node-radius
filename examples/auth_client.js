ccee9a67b8d81cf4a1358981a0210294a4eaf689
Muir Manders <muir@nearbuysystems.com>
2013-03-15 09:22:53 -0700

    Expand examples/auth_client.js a little.
    Now it uses the new verify_response method to make sure it can
    trust the response it gets from the server.

--- examples/auth_client.js
+++ examples/auth_client.js
@@ -6,8 +6,7 @@ var util = require('util');
 
 var secret = 'radius_secret';
 
-
-var packet_good = {
+var packet_accepted = {
   code: "Access-Request",
   secret: secret,
   identifier: 0,
@@ -18,7 +17,7 @@ var packet_good = {
   ]
 };
 
-var packet_bad = {
+var packet_rejected = {
   code: "Access-Request",
   secret: secret,
   identifier: 1,
@@ -29,22 +28,54 @@ var packet_bad = {
   ]
 };
 
+var packet_wrong_secret = {
+  code: "Access-Request",
+  secret: "wrong_secret",
+  identifier: 2,
+  attributes: [
+    ['NAS-IP-Address', '10.5.5.5'],
+    ['User-Name', 'riker'],
+    ['User-Password', 'Riker-Omega-3']
+  ]
+};
+
 var client = dgram.createSocket("udp4");
 
 client.bind(49001);
 
 var response_count = 0;
 
-client.on('message', function (msg, rinfo) {
+client.on('message', function(msg, rinfo) {
   var response = radius.decode({packet: msg, secret: secret});
-  var p = [packet_good, packet_bad].filter(function (packet) { return packet.identifier == response.identifier; });
-  console.log('Got ' + response.code + ' for ' + p[0].attributes[1][1]);
-  response_count++;
-  if (response_count > 1) client.close();
+  var request = sent_packets[response.identifier];
+
+  // although it's a slight hassle to keep track of packets, it's a good idea to verify
+  // responses to make sure you are talking to a server with the same shared secret
+  var valid_response = radius.verify_response({
+    response: msg,
+    request: request.raw_packet,
+    secret: request.secret
+  });
+  if (valid_response) {
+    console.log('Got valid response ' + response.code + ' for packet id ' + response.identifier);
+    // take some action based on response.code
+  } else {
+    console.log('WARNING: Got invalid response ' + response.code + ' for packet id ' + response.identifier);
+    // don't take action since server cannot be trusted (but maybe alert user that shared secret may be incorrect)
+  }
+
+  if (++response_count == 3) {
+    client.close();
+  }
 });
 
+var sent_packets = {};
 
-[packet_good, packet_bad].forEach(function(packet) {
+[packet_accepted, packet_rejected, packet_wrong_secret].forEach(function(packet) {
   var encoded = radius.encode(packet);
+  sent_packets[packet.identifier] = {
+    raw_packet: encoded,
+    secret: packet.secret
+  };
   client.send(encoded, 0, encoded.length, 1812, "localhost");
 });
